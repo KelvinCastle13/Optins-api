2c9af12c3e5cbe69f7117051434f4cab9a6dcdf2
KelvinCastle13 <kelvincastle13@gmail.com>
2025-08-03 21:47:21 -0400

    add sessions and current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   post "/signup" => "user#create"
 
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
